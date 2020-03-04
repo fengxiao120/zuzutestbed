ee8048c46dabbd25b332945e8333b2cb2035752a
Feng Xiao <feng@zuzuhs.com>
2020-03-04 11:35:24 +0800

    Update avail calendar

--- src/scenes/AvailCalendar/CalendarHeaderRow.js
+++ src/scenes/AvailCalendar/CalendarHeaderRow.js
@@ -5,24 +5,26 @@ const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 const mon = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 class CalendarHeaderRow extends React.PureComponent {
-  constructor(props) {
-    super(props)
-    this.state = {
-    }
-  }
-
   render() {
     const date = new Date()
     return (
       <React.Fragment>
-        { (this.props.skipped > 0) && <div style={{ gridColumnStart: this.props.skipped }}>&nbsp;</div>}
-        { this.props.rowData.map( (dayData, index) => <div 
-          key={index} 
-          className={(index === this.props.rowData.length - 1) ? 'last-column calendar-header-cell' : 'calendar-header-cell'}>
-           <div>{mon[new Date(date.getFullYear(), date.getMonth(), date.getDate() + dayData.date).getDay()]}</div>
-           <div className='bold'>{dayData.date}</div>
-          </div>)
-        }
+        {this.props.skipped > 0 && (
+          <div style={{ gridColumnStart: this.props.skipped }}>&nbsp;</div>
+        )}
+        {this.props.rowData.map((dayData, index) => (
+          <div
+            key={index}
+            className={
+              index === this.props.rowData.length - 1
+                ? 'last-column calendar-header-cell'
+                : 'calendar-header-cell'
+            }
+          >
+            <div>{days[dayData.date.getDay()]}</div>
+            <div className="bold">{dayData.date.getDate()}</div>
+          </div>
+        ))}
       </React.Fragment>
     )
   }
